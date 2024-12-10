4e3a3640015d2be0b675eb0db4aedbbae1ca5166
EmanuelBacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-12-10 20:06:16 -0300

    feat: add table modal component

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,13 +1,32 @@
+import { useState } from 'react';
 import { FlatList, SafeAreaView, StatusBar, Text, View } from 'react-native';
 import { Platform } from 'react-native';
 import { Button } from '../components/button';
 import { Categories } from '../components/categories';
 import { Header } from '../components/header';
 import { Menu } from '../components/menu';
+import { TableModal } from '../components/table-modal';
 
 export const Main = () => {
   const isAndroid = Platform.OS === 'android';
   const statusbarHeight = isAndroid ? StatusBar.currentHeight : 0;
+
+  const [isTableModalVisible, setIsTableModalVisible] =
+    useState<boolean>(false);
+
+  const [selectedTable, setSelectedTable] = useState<string>('');
+
+  const handleSaveTable = (table: string) => {
+    setSelectedTable(table);
+  };
+
+  const handleOpenTableModal = () => {
+    setIsTableModalVisible(true);
+  };
+
+  const handleCloseTableModal = () => {
+    setIsTableModalVisible(false);
+  };
 
   return (
     <>
@@ -28,11 +47,19 @@ export const Main = () => {
         </View>
 
         <View className="min-h-28 bg-white py-4 px-6">
-          <SafeAreaView>
-            <Button onPress={() => {}}>Novo pedido</Button>
-          </SafeAreaView>
+          {!selectedTable && (
+            <SafeAreaView>
+              <Button onPress={handleOpenTableModal}>Novo pedido</Button>
+            </SafeAreaView>
+          )}
         </View>
       </SafeAreaView>
+
+      <TableModal
+        onClose={handleCloseTableModal}
+        visible={isTableModalVisible}
+        onSave={handleSaveTable}
+      />
     </>
   );
 };
